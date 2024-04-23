7dd95bd22b7b4a3a34314de9e25b67329b1340ab
Rifkyalfia <165969362+Rifkyalfia@users.noreply.github.com>
2024-04-23 11:02:07 +0800

    Add files via upload

---
+++ rifkPphp/index.php
@@ -0,0 +1,30 @@
+<h1>List website pratikum</h1>
+<hr>
+<ul>
+    <li><a href="array1.php">Array 1</a></li>
+    <li><a href="array2.php">Array 2</a></li>
+    <li><a href="array3.php">Array 3</a></li>
+    <br>
+    <li><a href="function1.php">Function 1</a></li>
+    <li><a href="function1.php">Function 2</a></li>
+    <br>
+    <li><a href="kondisi3.php">Kondisi 3</a></li>
+    <li><a href="kondisi4.php">Kondisi 4</a></li>
+    <br>
+    <li><a href="perulangan1.php">Perulangan 1</a></li>
+    <li><a href="perulangan3.php">Perulangan 3</a></li>
+    <li><a href="perulangan4.php">Perulangan 4</a></li>
+    <li><a href="perulangan5.php">Perulangan 5</a></li>
+    <li><a href="perulangan6.php">Perulangan 6</a></li>
+    <br>
+    <li><a href="prak1.php">Praktek 1</a></li>
+    <li><a href="prak2.php">Praktek 2</a></li>
+    <li><a href="prak3.php">Praktek 3</a></li>
+    <li><a href="prak4.php">Praktek 4</a></li>
+    <li><a href="prak5.php">Praktek 5</a></li>
+
+
+   
+
+    
+
